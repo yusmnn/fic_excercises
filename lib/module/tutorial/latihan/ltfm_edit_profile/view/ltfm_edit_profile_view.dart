74d223b05c3e95f039296afc8e6c1417fd5ce109
yusmnn <yusmanqq@gmail.com>
2022-11-21 05:30:15 +0800

    update: 4 edit profile

--- lib/module/tutorial/latihan/ltfm_edit_profile/view/ltfm_edit_profile_view.dart
+++ lib/module/tutorial/latihan/ltfm_edit_profile/view/ltfm_edit_profile_view.dart
@@ -10,22 +10,59 @@ class LtfmEditProfileView extends StatefulWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("LtfmEditProfile"),
-        actions: const [
+        actions: [
           //! 4. Tambahkan tombol Save
           //! 5. Beri padding/margin pada tombol Save sebanyak 10
           //! 6. Panggil controller.save() ketika tombol di klik
+
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.save),
+              label: const Text("Save"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              onPressed: () async {
+                controller.save();
+              },
+            ),
+          ),
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: const [
+            children: [
               //! 1. Buat sebuah Card, tambahkan Column di dalamnya
               //! 2. Tambahkan padding.all 20.0
               //! 3. Di dalam column yang ada di dalam Card, tambahkan field ini:
               //? textfield email
               //? textfield password
+
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      QTextField(
+                        value: "user@gmail.com",
+                        label: "Email",
+                        hint: "Your email",
+                        onChanged: (value) {},
+                      ),
+                      QTextField(
+                        value: "123456",
+                        label: "Password",
+                        hint: "Your password",
+                        obscure: true,
+                        onChanged: (value) {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
